94d5439d325a1ade95fd81e11e286515c72ee5cc
MinseongJeon-SCU <minseong9078@gmail.com>
2024-11-22 00:28:55 +1000

    Working on Part 3
    In addition to adding a Queue, you need to add the following methods to the Ride class that work
    with the Queue:
    • A method named AddVisitorToQueue to add a Visitor to the Queue (see the interface
    you created in Part 2).
    • A method named RemoveVisitorFromQueue to remove a Visitor from the Queue (see the
    interface you created in Part 2).
    • A method named PrintQueue that prints all the details for all Visitors in the Queue in the
    order they were added (see the interface you created in Part 2).
    Note: Make sure all the above methods print suitable success/failure messages.
    Demonstration
    In the partThree method in the AssignmentTwo class:
    • Create a new Ride object.
    • Using the methods you created:
    ▪ Add a minimum of 5 Visitor to the Queue.
    5
    Assessment Brief
    ▪ Remove a Visitor from the Queue.
    ▪ Print all Visitors in the Queue

--- AssignmentTwo/src/AssignmentTwo.java
+++ AssignmentTwo/src/AssignmentTwo.java
@@ -1,25 +1,57 @@
-//Class
 public class AssignmentTwo {
+
+  
     public static void main(String[] args) {
+      AssignmentTwo assignment = new AssignmentTwo();
+      assignment.partThree();
     }
 
     public void partThree(){
+      Employee rideOperator = new Employee ("Minseong", "Male", 29, "Security", 11);
+      Ride R1 = new Ride ("SuperMan RollerCoaster", 10, rideOperator);
+
+      Visitor v1 = new Visitor ("John", "Male", 31, "Gold", 1);
+      Visitor v2 = new Visitor ("Park", "Female", 30, "Bronze", 2);
+      Visitor v3 = new Visitor ("Kim", "Female", 33, "Silver", 3);
+      Visitor v4 = new Visitor ("Eun", "Female", 31, "Gold", 4);
+      Visitor v5 = new Visitor ("Hwan", "Female", 36, "Bronze", 5);
+
+     R1.addVisitorToQueue(v1);
+     R1.addVisitorToQueue(v2);
+     R1.addVisitorToQueue(v3);
+     R1.addVisitorToQueue(v4);
+     R1.addVisitorToQueue(v5);
+
+     R1.printQueue();
+
+     //R1.removeVisitorFromQueue(v3); 
+
+     R1.printQueue();
 
     }
+
     public void partFourA(){
 
     }
-    public void partFourB(){
 
+    public void partFourB(){
     }
+
+
     public void partFive(){
 
     }
+
     public void partSix(){
 
     }
+
     public void partSeven(){
 
     }
+
+
     }
     
+
+
---
+++ AssignmentTwo/src/Ride.java
@@ -0,0 +1,84 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+
+//Class
+public class Ride implements RideInterface {
+    private String rideName;
+    private int rideWaitingTime;
+    private Queue<Visitor> waitingLine;
+    private Employee rideOperator;
+    
+    
+
+    //Default Constructor
+    public Ride () {
+        this.waitingLine = new LinkedList<>();
+    }
+
+    //Second Constructor
+    public Ride (String rideName, int rideWaitingTime, Employee rideOperator) {
+        this.rideName = rideName;
+        this.rideWaitingTime = rideWaitingTime;
+        this.rideOperator = rideOperator;
+        this.waitingLine = new LinkedList<>();
+    }
+
+    //Getter and Setter
+    public String getRideName () {
+        return rideName;
+    }
+
+    public void setRideName (String rideName) {
+        this.rideName = rideName;
+    }
+
+    public int getRideWaitingTime () {
+        return rideWaitingTime;
+    }
+
+    public void setRideWaitingTime (int rideWaitingTime) {
+        this.rideWaitingTime = rideWaitingTime;
+    }
+
+    public Employee getRideOperator () {
+        return rideOperator;
+    }
+
+    public void setRideOperator (Employee rideOperator) {
+        this.rideOperator = rideOperator;
+    }
+
+    @Override
+    public void addVisitorToQueue (Visitor visitor) {
+        if (visitor != null) { 
+            waitingLine.add(visitor); 
+            System.out.println(visitor.getName() + " has been added to the queue.");
+        } else {
+            System.out.println("No visitors in the queue");
+        }
+    }
+
+    @Override
+    public void removeVisitorFromQueue() {
+        if (!waitingLine.isEmpty()) {
+            Visitor removedVisitor = waitingLine.poll(); 
+            System.out.println(removedVisitor.getName() + " has been removed from the queue.");
+        } else {
+            System.out.println("Queue is empty.");
+        }
+    }
+
+    @Override
+    public void printQueue() {
+        if (waitingLine.isEmpty()) {
+            System.out.println("There are no visitors in the queue for the " + this.rideName);
+        } else {
+            System.out.println("Visitor is in the queue for the " + this.rideName);
+            for (Visitor visitor : waitingLine) {
+                System.out.println("Visitor Name-" + visitor.getName() + " Visitor Gender-" + visitor.getGender() + " Visitor Age-" +
+                visitor.getAge() + " Membership Type-" + visitor.getMembershipType() + " Park Member ID-" + visitor.getParkMemberID());
+            }
+        }
+    }
+}
